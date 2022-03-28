b79abfd31c0bf8c0f2b778f6c652faff6ccf200a
Elias Eriksson <elias@issuefab.com>
2022-03-28 16:51:15 +0200

    checkpoint

---
+++ stencil.config.ts
@@ -0,0 +1,20 @@
+import { Config } from "@stencil/core"
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+	globalStyle: "ui/src/global/app.css",
+	// globalScript: "ui/src/global/app.ts",
+	taskQueue: "async",
+	srcDir: "ui/src",
+	tsconfig: "tsconfig.ui.json",
+	outputTargets: [
+		{
+			type: "www",
+			// comment the following line to disable service workers in production
+			serviceWorker: null,
+			baseUrl: "https://myapp.local/",
+			dir: "dist/ui/",
+		},
+	],
+}
